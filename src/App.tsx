79cc93a1b68e2138a012f93439731badf23642f5
Todd Grilliot <tbgrilliot@gmail.com>
2024-04-10 14:59:52 -0600

    Init and some cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App-header flex flex-col justify-start items-start min-h-screen text-white bg-sky-950">
+            <div>
+                <h1 className="text-3xl font-bold text-white">Hello World!</h1>
+            </div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p className="py-12">Hello World!</p>
+        </div>
+    );
 }
 
 export default App;
